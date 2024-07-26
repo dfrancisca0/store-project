057ef48f796f6786b9b065277bb79dd021d9e38f
dfrancisca0 <d.bustosgavilan@gmail.com>
2024-07-26 14:01:30 +0200

    Layout admin pannel | form component | table component | menu component

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -21,89 +21,19 @@ class Admin extends HTMLElement {
       
       <style>
 
-        h2, span {
+        h2, span, label {
           margin: 0;
           font-family: ;
-          color: hsla(0, 0%, 100%, 1);
+          color: hsla(0, 100%, 100%, 1);
           font-family: "Lato", sans-serif;
-          font-size: 1rem;
-        }
-
-        ul {
-          list-style: none;
-          padding: 0
-        }
-
-        input {
-          width: 40%
-        }
-
-        input::placeholder {
-          color:hsl(0, 0%, 0%)
-        }
-
-        button {
-          width: 45%
-        }
-
-        button {
-          padding: .5rem;
-          background-color:hsl(0, 0%, 100%);
-          font-weight: 600;
-          border: none;
-          border-radius: 5px;
-          cursor: pointer
+          font-size: .9rem;
         }
 
         .admin {
-          padding: 1.5rem 1rem
-        }
-
-        form {
-          display: flex;
-          flex-direction: column;
-          gap: 1rem
-        }
-
-        .admin-reference {
-          display: flex;
-          justify-content: space-between;
-        }
-
-        .admin-date {
-          display: flex;
-          justify-content: space-between
-        }
-
-        .admin-list li {
-          padding: 1rem;
-          border-top: 2px solid hsl(0, 0%, 100%)
-        }
-
-        .admin-product {
-          display: flex;
-          flex-direction: column;
-          gap: 1rem
-        }
-
-        .admin-product-main {
-          display: flex;
-          justify-content: space-between;
-        }
-
-        .admin-product-main span {
-          font-size: 1.2rem;
-          font-weight: 600
-        }
-
-        .admin-product-secondary {
-          display: flex;
-          justify-content: space-between
-        }
-        
-        .admin-product-secondary button {
-          width: 30%;
-          padding: .3rem
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+          gap: 2rem;
+          padding: 2rem
         }
 
         @media (hover: hover) {
@@ -116,78 +46,11 @@ class Admin extends HTMLElement {
 
       </style>
 
-      <section class="admin"></section>
+      <section class="admin">
+        <slot></slot>
+      </section>
       `
-    const admin = this.shadow.querySelector('.admin')
-    const form = document.createElement('form')
-    admin.appendChild(form)
-
-    const adminReference = document.createElement('div')
-    adminReference.classList.add('admin-reference')
-    form.appendChild(adminReference)
-
-    const referenceInputElement = document.createElement('input')
-    referenceInputElement.setAttribute('type', 'number')
-    referenceInputElement.setAttribute('name', 'number')
-    referenceInputElement.setAttribute('placeholder', 'Referencia del pedido')
-    adminReference.appendChild(referenceInputElement)
-
-    const referenceButtonElement = document.createElement('button')
-    referenceButtonElement.textContent = 'Buscar por referencia'
-    adminReference.appendChild(referenceButtonElement)
-
-    const adminDate = document.createElement('div')
-    adminDate.classList.add('admin-date')
-    form.appendChild(adminDate)
-
-    const dateInputElement = document.createElement('input')
-    dateInputElement.setAttribute('type', 'date')
-    dateInputElement.setAttribute('name', 'date')
-    adminDate.appendChild(dateInputElement)
-
-    const dateButtonElement = document.createElement('button')
-    dateButtonElement.textContent = 'Buscar por fecha'
-    adminDate.appendChild(dateButtonElement)
-
-    const adminList = document.createElement('div')
-    adminList.classList.add('admin-list')
-    admin.appendChild(adminList)
-
-    const adminListElement = document.createElement('ul')
-    adminList.appendChild(adminListElement)
-
-    this.data.forEach(element => {
-      const listElement = document.createElement('li')
-      adminListElement.appendChild(listElement)
-
-      const adminProduct = document.createElement('div')
-      adminProduct.classList.add('admin-product')
-      listElement.appendChild(adminProduct)
-
-      const adminProductMain = document.createElement('div')
-      adminProductMain.classList.add('admin-product-main')
-      adminProduct.appendChild(adminProductMain)
-
-      const productMainElement = document.createElement('span')
-      productMainElement.textContent = element.reference
-      adminProductMain.appendChild(productMainElement)
-
-      const productMainTotalElement = document.createElement('span')
-      productMainTotalElement.textContent = `${element.total}€`
-      adminProductMain.appendChild(productMainTotalElement)
-
-      const adminProductSecondary = document.createElement('div')
-      adminProductSecondary.classList.add('admin-product-secondary')
-      adminProduct.appendChild(adminProductSecondary)
-
-      const productSecondaryElement = document.createElement('span')
-      productSecondaryElement.textContent = `${element.date} ${element.time}`
-      adminProductSecondary.appendChild(productSecondaryElement)
-
-      const productSecondaryButtonElement = document.createElement('button')
-      productSecondaryButtonElement.textContent = 'Ver pedido'
-      adminProductSecondary.appendChild(productSecondaryButtonElement)
-    })
+    
   }
 }
 
--- client/src/components/menu-link.js
+++ client/src/components/menu-link.js
@@ -20,7 +20,7 @@ class MenuLink extends HTMLElement {
         }
 
         .menu-link svg{
-          fill: hsl(0, 0%, 100%);
+          fill: hsl(0, 100%, 100%);
           display: flex;
           height: 2rem;
           width: 2rem;
@@ -30,7 +30,7 @@ class MenuLink extends HTMLElement {
       </style>
 
       <button class="menu-link">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><title>apps</title><path d="M7 7H3V3H7M13 7H9V3H13M19 7H15V3H19M7 13H3V9H7M13 13H9V9H13M19 13H15V9H19M7 19H3V15H7M13 19H9V15H13M19 19H15V15H19Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><title>apps</title><path d="M7 7H3V3H7M13 7H9V3H13M19 7H15V3H19M7 13H3V9H7M13 13H9V9H13M19 13H15V9H19M7 19H3V15H7M13 19H9V15H13M19 19H15V15H19Z" /></svg>
       </button>
     `
   }
